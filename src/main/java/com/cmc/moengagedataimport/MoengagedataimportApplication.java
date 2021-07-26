0e0516fc374fff89258dd175b043e58c3a073261
Lam Nguyen <lnguye16@gustavus.edu>
2021-07-26 09:37:30 +0700

    first commit

---
+++ src/main/java/com/cmc/moengagedataimport/MoengagedataimportApplication.java
@@ -0,0 +1,13 @@
+package com.cmc.moengagedataimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoengagedataimportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoengagedataimportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmc/moengagedataimport/MoengagedataimportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.moengagedataimport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoengagedataimportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
